7451d1885bfdad2baf053c8c0a13d76c4fca254f
marianmna <solovevamaria242@gmail.com>
2024-02-27 23:04:21 +0700

    change1

--- task.py
+++ task.py
@@ -1,20 +1,35 @@
-import math
-V_lake = int(input("Введите объем озера в м^3"))
-m = float(input("Введите массу груза (кг): "))
-k = float(input("Введите коэффициент жесткости пружины (Н/м): "))
-A = V_lake
-t = float(input("Введите момент времени (с): "))
-long_moon = 384467000
+# Case-study #2: pillar
+# Developer:
+#
 
-if (long_moon % V_lake != 0):
-    lake_for_moon = long_moon // V_lake + 1
-else:
-    lake_for_moon = long_moon // V_lake
+import local_ru as ru
+def main():
+    '''
+    Main function.
+    :return: None
+    '''
+
+    import math
+    V_lake = int(input(ru.V_lake))
+    m = float(input(ru.m))
+    k = float(input(ru.k))
+    A = V_lake
+    t = float(input(ru.t))
+    long_moon = 384467000
+
+    if (long_moon % V_lake != 0):
+        lake_for_moon = long_moon // V_lake + 1
+    else:
+        lake_for_moon = long_moon // V_lake
 
 
-T = 2 * math.pi * (m / k)  0.5
-v = A * (2 * math.pi / T) * math.cos(2 * math.pi * t / T)
-a = -A * (2 * math.pi / T)  2 * math.sin(2 * math.pi * t / T)
-print(long_moon// V_lake)
-print(f"Скорость груза в момент времени {t} сек: {v} м/с")
-print(f"Максимальное ускорение груза в момент времени {t} сек: {a} м/с^2")
+    T = 2 * math.pi * (m / k)** 0.5
+    v = A * (2 * math.pi / T) * math.cos(2 * math.pi * t / T)
+    a = -A * (2 * math.pi / T)** 2 * math.sin(2 * math.pi * t / T)
+    print(long_moon// V_lake)
+    print(f"{ru.load_speed} {t} {ru.sec} {v} {ru.mc}")
+    print(f"{ru.load_acceleration} {t} {ru.sec} {a} {ru.mcc}")
+
+
+if __name__ == '__main__':
+    main()
